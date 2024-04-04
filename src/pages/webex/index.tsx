e42ed410527905927215d2b7b0bfd18272abcc01
Le Van Vinh <vinh5.le@lge.com>
2024-04-04 15:28:21 +0700

    test save db

--- src/pages/webex/index.tsx
+++ src/pages/webex/index.tsx
@@ -14,24 +14,24 @@ const WebexPage = () => {
     async function backToApp() {
       if (code) {
         //@ts-ignore
-        new window.PalmServiceBridge().call("luna://com.palm.db/merge", JSON.stringify({"objects":[{"_id":"persist:roo","value":"{\"account\":\"{\\\"code\\\":\\\"'+code+'\\\"}\",\"_persist\":\"{\\\"version\\\":-1,\\\"rehydrated\\\":true}\"}","type":"string","_kind":"webexsample:1"}]}));
+        new window.PalmServiceBridge().call("luna://com.palm.db/merge", JSON.stringify({"objects":[{"_id":"persist:roo","value":"{\"account\":\"{\\\"code\\\":\\\""+code+"\\\"}\",\"_persist\":\"{\\\"version\\\":-1,\\\"rehydrated\\\":true}\"}","type":"string","_kind":"webexsample:1"}]}));
 
         // while(!location.href.startsWith("file://")) {
         //     await delay(10)
         //     history.back();
         // }
         // await delay(10)
-        // //@ts-ignore
-        // new window.PalmServiceBridge().call(
-        //   "luna://com.webos.applicationManager/closeByAppId",
-        //   `{"id":"test-login-webex"}`
-        // );
-        // await delay(1000);
-        // //@ts-ignore
-        // new window.PalmServiceBridge().call(
-        //   "luna://com.webos.applicationManager/launch",
-        //   `{"id":"test-login-webex", "params": {"code":"${code}"}}`
-        // );
+        //@ts-ignore
+        new window.PalmServiceBridge().call(
+          "luna://com.webos.applicationManager/closeByAppId",
+          `{"id":"test-login-webex"}`
+        );
+        await delay(1000);
+        //@ts-ignore
+        new window.PalmServiceBridge().call(
+          "luna://com.webos.applicationManager/launch",
+          `{"id":"test-login-webex", "params": {"code":"${code}"}}`
+        );
       }
     }
     backToApp();
